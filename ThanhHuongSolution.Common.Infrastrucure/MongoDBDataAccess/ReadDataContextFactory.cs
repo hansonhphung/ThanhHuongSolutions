4d16b7154f73580e490a98feb52c45b58d2fbaa2
Hung Phung <hungphung0808@gmail.com>
2015-12-01 01:00:27 +0700

    Implement read and write MongoDB data context factory

---
+++ ThanhHuongSolution.Common.Infrastrucure/MongoDBDataAccess/ReadDataContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThanhHuongSolution.Common.Infrastrucure.MongoDBDataAccess
+{
+    public class ReadDataContextFactory : IReadDataContextFactory
+    {
+        private readonly MongoClient _mongoDBClient;
+        private readonly string _connectionString;
+
+        public ReadDataContextFactory(string connectionString)
+        {
+            Check.NotEmpty(connectionString, "connectionString");
+
+            _connectionString = connectionString;
+
+            _mongoDBClient = new MongoClient(connectionString);
+        }
+
+        public IMongoDatabase CreateMongoDBReadContext()
+        {
+            var dbName = new MongoUrl(_connectionString).DatabaseName;
+
+            return _mongoDBClient.GetDatabase(dbName);
+        }
+    }
+}
