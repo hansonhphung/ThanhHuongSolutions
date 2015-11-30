e01afc17939a5d8bbc0989a634a23a192eca84a8
Hung Phung <hungphung0808@gmail.com>
2015-12-01 01:03:20 +0700

    Add MongoDB Data Access class for Customer Management

---
+++ ThanhHuongSolution.Customer.MongoDBDataAccess/CustomerRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThanhHuongSolution.Common.Infrastrucure;
+using ThanhHuongSolution.Common.Infrastrucure.MongoDBDataAccess;
+
+namespace ThanhHuongSolution.Customer.MongoDBDataAccess
+{
+    public class CustomerRepository
+    {
+        private readonly IReadDataContextFactory _readDataContectFactory;
+        private readonly IWriteDataContextFactory _writeDataContextFactory;
+
+        public CustomerRepository(IReadDataContextFactory readDataContextFactory, IWriteDataContextFactory writeDataContextFactory)
+        {
+            Check.NotNull(readDataContextFactory, "readDataContextFactory");
+            Check.NotNull(writeDataContextFactory, "writeDataContextFactory");
+
+            _readDataContectFactory = readDataContextFactory;
+            _writeDataContextFactory = writeDataContextFactory;
+        }
+    }
+}
