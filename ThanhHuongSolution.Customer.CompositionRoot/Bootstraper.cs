4f5cb00ef2f36bb9ef724175c58607cf950d7acd
Hung Phung <hungphung0808@gmail.com>
2015-12-01 01:06:39 +0700

    Implement Composition root for Customer Management and Regist DI

---
+++ ThanhHuongSolution.Customer.CompositionRoot/Bootstraper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThanhHuongSolution.Common.Infrastrucure;
+
+namespace ThanhHuongSolution.Customer.CompositionRoot
+{
+    public class Bootstraper
+    {
+        public static void Load(IObjectContainer objectContainer)
+        {
+            DIRegister.RegisterRepositories(objectContainer);
+        }
+    }
+}
