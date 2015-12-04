c67b297dac0da3a3c047f923aee43d85586bc781
Hung Phung <hungphung0808@gmail.com>
2015-12-05 02:05:25 +0700

    Add MongoDB Entity Names to store MongoDB collection name

---
+++ ThanhHuongSolution.Common.Infrastrucure/MongoDBDataAccess/Entity/MongoDBEntityNames.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThanhHuongSolution.Common.Infrastrucure.MongoDBDataAccess.Entity
+{
+    public class MongoDBEntityNames
+    {
+        public string TableName { get; set; }
+
+        #region Customer
+        public static readonly MongoDBEntityNames CustomerCollection = new MongoDBEntityNames() { TableName = "Customers" };
+        #endregion
+
+        public override string ToString()
+        {
+            return TableName;
+        }
+    }
+}
