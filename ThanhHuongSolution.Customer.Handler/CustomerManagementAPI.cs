abdcc9e4afc3e0bb8337f21fce6d2393470acdc3
Hung Phung <hungphung0808@gmail.com>
2015-12-05 03:59:32 +0700

    Implement customer services and APIs

---
+++ ThanhHuongSolution.Customer.Handler/CustomerManagementAPI.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThanhHuongSolution.Common.Infrastrucure;
+using ThanhHuongSolution.Customer.Domain.Interfaces;
+using ThanhHuongSolution.Customer.Domain.Model;
+
+namespace ThanhHuongSolution.Customer.Handler
+{
+    public class CustomerManagementAPI : ICustomerManagementAPI
+    {
+        public IObjectContainer _objectContainer;
+
+        public CustomerManagementAPI(IObjectContainer objectContainer)
+        {
+            _objectContainer = objectContainer;
+        }
+
+        public async Task<FrameworkParamOutput<CustomerInfo>> CreateCustomer(FrameworkParamInput<CustomerInfo> input)
+        {
+            try
+            {
+                var request = input.Request;
+
+                var services = _objectContainer.Get<ICustomerManagementServices>();
+
+                var data = await services.CreateCustomer(request);
+
+                return await Task.FromResult(new FrameworkParamOutput<CustomerInfo>(data));
+            }
+            catch (CustomException ex)
+            {
+                throw new CustomException(ex);
+            }
+        }
+
+        public async Task<FrameworkParamOutput<IList<CustomerInfo>>> GetAllCustomer()
+        {
+            try
+            {
+                var services = _objectContainer.Get<ICustomerManagementServices>();
+
+                var data = await services.GetAllCustomer();
+
+                return await Task.FromResult(new FrameworkParamOutput<IList<CustomerInfo>>(data));
+            }
+            catch (CustomException ex)
+            {
+                throw new CustomException(ex);
+            }
+        }
+    }
+}
--- ThanhHuongSolution.Customer.Services/CustomerManagementServices.cs
+++ ThanhHuongSolution.Customer.Services/CustomerManagementServices.cs
@@ -4,17 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ThanhHuongSolution.Common.Infrastrucure;
+using ThanhHuongSolution.Customer.Domain.Interfaces;
+using ThanhHuongSolution.Customer.Domain.Model;
 using ThanhHuongSolution.Customer.MongoDBDataAccess;
+using ThanhHuongSolution.Common.LocResources;
 
 namespace ThanhHuongSolution.Customer.Services
 {
-    public class CustomerManagementServices
+    public class CustomerManagementServices : ICustomerManagementServices
     {
         public IObjectContainer _objectContainer;
 
         public CustomerManagementServices(IObjectContainer objectContainer)
         {
             _objectContainer = objectContainer;
+        }
+
+        public async Task<CustomerInfo> CreateCustomer(CustomerInfo customer)
+        {
+            var repository = _objectContainer.Get<ICustomerRepository>();
+
+            var mdCustomer = customer.GetEntity();
+
+            var result = await repository.CreateCustomer(mdCustomer);
+
+            return await Task.FromResult<CustomerInfo>(new CustomerInfo(result));
+        }
+
+        public async Task<IList<CustomerInfo>> GetAllCustomer()
+        {
+            var repository = _objectContainer.Get<ICustomerRepository>();
+
+            var data = await repository.GetAllCustomer();
+
+            Check.ThrowExceptionIfCollectionIsNullOrZero(data, CustomerManagementResources.NO_CUSTOMER);
+
+            var result = new List<CustomerInfo>();
+
+            foreach (var customer in data)
+            {
+                result.Add(new CustomerInfo(customer));
+            }
+
+            return await Task.FromResult<IList<CustomerInfo>>(result);
         }
     }
 }
