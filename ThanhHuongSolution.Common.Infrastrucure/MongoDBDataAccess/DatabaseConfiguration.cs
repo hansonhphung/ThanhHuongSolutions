a32fe67bf2465f215c70b9939e9627df929e0215
Hung Phung <hungphung0808@gmail.com>
2015-12-05 01:47:38 +0700

    Add system.configuration dll and implement database Configuration

---
+++ ThanhHuongSolution.Common.Infrastrucure/MongoDBDataAccess/DatabaseConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ThanhHuongSolution.Common.Infrastrucure.MongoDBDataAccess
+{
+    public class DatabaseConfiguration
+    {
+        public static string MongoDbConnection
+        {
+            get { return ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString; }
+        }
+    }
+}
