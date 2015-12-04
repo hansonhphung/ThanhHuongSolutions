31b59c7c49fb6121eb2b6f17dbfa7b11b1fbcace
Hung Phung <hungphung0808@gmail.com>
2015-12-05 03:59:02 +0700

    Create customer entity and model

---
+++ ThanhHuongSolution.Customer.Domain/Entity/MDCustomer.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThanhHuongSolution.Customer.Domain.Entity
+{
+    public class MDCustomer
+    {
+        public MDCustomer()
+        { }
+
+        public MDCustomer(string trackingNumber, string name, string phoneNumber, string address, List<string> transactionDetailIds, long liabilityAmount)
+        {
+            TrackingNumber = trackingNumber;
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Address = address;
+            TransactionDetailIds = transactionDetailIds;
+            LiabilityAmount = liabilityAmount;
+        }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string TrackingNumber { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public List<string> TransactionDetailIds { get; set; }
+        public long LiabilityAmount { get; set; }
+    }
+}
---
+++ ThanhHuongSolution.Customer.Domain/Model/CustomerInfo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThanhHuongSolution.Customer.Domain.Entity;
+
+namespace ThanhHuongSolution.Customer.Domain.Model
+{
+    public class CustomerInfo
+    {
+        public CustomerInfo() { }
+
+        public CustomerInfo(string trackingNumber, string name, string phoneNumber, string address, List<string> transactionDetailIds, long liabilityAmount)
+        {
+            TrackingNumber = trackingNumber;
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Address = address;
+            TransactionDetailIds = transactionDetailIds;
+            LiabilityAmount = liabilityAmount;
+        }
+
+        public CustomerInfo(MDCustomer mdCustomer)
+        {
+            Id = mdCustomer.Id;
+            TrackingNumber = mdCustomer.TrackingNumber;
+            Name = mdCustomer.Name;
+            PhoneNumber = mdCustomer.PhoneNumber;
+            Address = mdCustomer.Address;
+            TransactionDetailIds = mdCustomer.TransactionDetailIds;
+            LiabilityAmount = mdCustomer.LiabilityAmount;
+        }
+
+        public MDCustomer GetEntity()
+        {
+            return new 
+                MDCustomer(TrackingNumber, 
+                Name, 
+                PhoneNumber, 
+                Address, 
+                TransactionDetailIds, 
+                LiabilityAmount);
+        }
+
+        public string Id { get; set; }
+        public string TrackingNumber { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public List<string> TransactionDetailIds { get; set; }
+        public long LiabilityAmount { get; set; }
+    }
+}
