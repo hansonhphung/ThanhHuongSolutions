b2648081d7a2c0b36fc2c8ff468e78223f3cdaa4
Hung Phung <hungphung0808@gmail.com>
2015-12-06 23:36:04 +0700

    Add unit test project for CustomerManagement Bounded-context

---
+++ ThanhHuongSolution.Customer.UnitTests/CustomerManagementAPITests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThanhHuongSolution.Customer.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThanhHuongSolution.Common.Infrastrucure;
+using ThanhHuongSolution.Customer.UnitTests;
+
+namespace ThanhHuongSolution.Customer.Handler
+{
+    [TestClass]
+    public class CustomerManagementAPITests
+    {
+        private IObjectContainer _objectContainer;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _objectContainer = UnitTestHelper.SetupKernelBootstraper();
+
+            var api = _objectContainer.Get<ICustomerManagementAPI>();
+
+            var result = api.CreateCustomer(new FrameworkParamInput<Domain.Model.CustomerInfo>(new CustomerInfo()));
+        }
+
+        [TestMethod]
+        public void CustomerManagementAPITest()
+        {
+            
+        }
+    }
+}
---
+++ ThanhHuongSolution.Customer.UnitTests/UnitTestHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThanhHuongSolution.Common.Infrastrucure;
+using ThanhHuongSolution.Common.Infrastrucure.Utilities;
+
+namespace ThanhHuongSolution.Customer.UnitTests
+{
+    public class UnitTestHelper
+    {
+        public static IObjectContainer SetupKernelBootstraper()
+        {
+            var objectContainer = DIHelper.CreateObjectContainer();
+
+            CompositionRoot.Bootstraper.Load(objectContainer);
+
+            return objectContainer;
+        }
+    }
+}
