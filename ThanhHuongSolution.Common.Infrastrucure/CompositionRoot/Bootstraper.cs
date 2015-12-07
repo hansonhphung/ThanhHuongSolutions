b1743331ac68da12b78b4693d0885387beb8ea0a
Hung Phung <hungphung0808@gmail.com>
2015-12-07 22:52:03 +0700

    Implement DIRegister to regist ninject for Common.Infrastructure

---
+++ ThanhHuongSolution.Common.Infrastrucure/CompositionRoot/Bootstraper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThanhHuongSolution.Common.Infrastrucure.CompositionRoot
+{
+    public class Bootstraper
+    {
+        public static void Load(IObjectContainer objectContainer)
+        {
+            DIRegister.RegisterDataContextFactory(objectContainer);
+
+            DIRegister.RegisterObjectContainer(objectContainer);
+        }
+    }
+}
---
+++ ThanhHuongSolution.Common.Infrastrucure/CompositionRoot/DIRegister.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThanhHuongSolution.Common.Infrastrucure.MongoDBDataAccess;
+
+namespace ThanhHuongSolution.Common.Infrastrucure.CompositionRoot
+{
+    public class DIRegister
+    {
+        public static void RegisterDataContextFactory(IObjectContainer objectContainer)
+        {
+            var connectionStr = DatabaseConfiguration.MongoDbConnection;
+
+            objectContainer.BindWithConstructorArgument<ReadDataContextFactory>(typeof(IReadDataContextFactory), connectionStr);
+
+            objectContainer.BindWithConstructorArgument<WriteDataContextFactory>(typeof(IWriteDataContextFactory), connectionStr);
+        }
+
+        public static void RegisterObjectContainer(IObjectContainer objectContainer)
+        {
+            objectContainer.BindTo<IObjectContainer, ObjectContainer>();
+        }
+    }
+}
