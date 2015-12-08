a191841673a7b8c90579ce7085d3fb35aa21daba
Hung Phung <hungphung0808@gmail.com>
2015-12-08 16:50:16 +0700

    Add Backend Build Trigger project to trigger build all neccessary backend dlls

---
+++ ThanhHuongSolution.Backend.BuildTrigger/Readme.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThanhHuongSolution.Backend.BuildTrigger
+{
+    public class ReadMe
+    {
+        //This is a temporary project that trigger the build of all necessary project
+        //The out put of this project are placed in packages\Backend_dlls
+        //The build dependencies of Web Main are configured to build this project first
+    }
+}
