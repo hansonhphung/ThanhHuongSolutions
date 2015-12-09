31a054b3172a9c40efc55e8a06c72f13cef88dbf
Hung Phung <hungphung0808@gmail.com>
2015-12-10 00:33:23 +0700

    Add test Model

---
+++ ThanhHuongSolution/Models/TestModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThanhHuongSolution.Models
+{
+    public class TestModel
+    {
+        public List<string> CustomerName { get; set; }
+    }
+}
