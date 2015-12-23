14dc2e409d66063c4cecf2f2c0bf7fcee4323d76
Hung Phung <hungphung0808@gmail.com>
2015-12-24 00:52:12 +0700

    Add front-end customer controller

---
+++ ThanhHuongSolution/Scripts/Customer/CustomerController.js
@@ -0,0 +1,22 @@
+﻿var app = angular.module('ThanhHuongSolution', []);
+app.controller('CustomerController', function ($scope) {
+    $scope.init = function (data) {
+        $scope.customers = data.LstCustomer;
+    }
+
+    $scope.viewCustomer = function (customerId)
+    {
+        for (var i = 0; i < $scope.customers.length; i++)
+        {
+            if ($scope.customers[i].Id == customerId)
+            {
+                $scope.trackingNumber = $scope.customers[i].TrackingNumber;
+                $scope.name = $scope.customers[i].Name;
+                $scope.phoneNumber = $scope.customers[i].PhoneNumber;
+                $scope.address = $scope.customers[i].Address;
+                $scope.liabilityAmount = $scope.customers[i].LiabilityAmount;
+                return;
+            }
+        }
+    }
+});
