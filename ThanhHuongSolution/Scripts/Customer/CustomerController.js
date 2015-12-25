1d4210db3c5fc21160681f170ce179c485366692
Hung Phung <hungphung0808@gmail.com>
2015-12-26 01:34:03 +0700

    Apply client search for Customer instead of server Search

--- ThanhHuongSolution/Scripts/Customer/CustomerController.js
+++ ThanhHuongSolution/Scripts/Customer/CustomerController.js
@@ -1,22 +1,66 @@
 ﻿var app = angular.module('ThanhHuongSolution', []);
 app.controller('CustomerController', function ($scope) {
     $scope.init = function (data) {
+        $scope.availableCustomer = data.LstCustomer;
         $scope.customers = data.LstCustomer;
     }
 
     $scope.viewCustomer = function (customerId)
     {
-        for (var i = 0; i < $scope.customers.length; i++)
-        {
-            if ($scope.customers[i].Id == customerId)
-            {
-                $scope.trackingNumber = $scope.customers[i].TrackingNumber;
-                $scope.name = $scope.customers[i].Name;
-                $scope.phoneNumber = $scope.customers[i].PhoneNumber;
-                $scope.address = $scope.customers[i].Address;
-                $scope.liabilityAmount = $scope.customers[i].LiabilityAmount;
-                return;
+        $scope.customerId = customerId;
+        if (customerId != '') {
+            for (var i = 0; i < $scope.customers.length; i++) {
+                if ($scope.customers[i].Id == customerId) {
+                    $scope.trackingNumber = $scope.customers[i].TrackingNumber;
+                    $scope.name = $scope.customers[i].Name;
+                    $scope.phoneNumber = $scope.customers[i].PhoneNumber;
+                    $scope.address = $scope.customers[i].Address;
+                    $scope.liabilityAmount = $scope.customers[i].LiabilityAmount;
+                    return;
+                }
             }
+        }
+        else {
+            $scope.trackingNumber = ''
+            $scope.name = '';
+            $scope.phoneNumber = '';
+            $scope.address = '';
+            $scope.liabilityAmount = '';
+        }
+    }
+
+    //$scope.searchCustomer = function()
+    //{
+    //    var query = $scope.query;
+
+    //    $.ajax({
+    //        type: "POST",
+    //        url: "/Customer/Search",
+    //        data: { query: query },
+    //        success: function (response) {
+    //            if (response.isSuccess) {
+    //                var data = response.data;
+    //                $scope.customers = data;
+    //            }
+    //            else {
+    //                alert('error at: ' + response.message);
+    //            }
+    //        }
+    //    });
+    //}
+
+    $scope.searchCustomer = function ()
+    {
+        $scope.customers = [];
+        var query = $scope.query.toLowerCase();
+
+        for (var i = 0; i < $scope.availableCustomer.length; i++)
+        {
+            var trackingLower = $scope.availableCustomer[i].TrackingNumber.toLowerCase();
+            var nameLower = $scope.availableCustomer[i].Name.toLowerCase();
+
+            if (trackingLower.indexOf(query) !== -1 || nameLower.indexOf(query) !== -1)
+                $scope.customers.push($scope.availableCustomer[i]);
         }
     }
 
@@ -24,24 +68,7 @@ app.controller('CustomerController', function ($scope) {
     {
         if (event.keyCode == 13)
         {
-            var query = $scope.query;
-            
-            $.ajax({
-                type: "POST",
-                url: "/Customer/Search",
-                data: {query : query},
-                success: function (response) {
-                    if (response.isSuccess)
-                    {
-                        var data = response.data;
-                        alert(JSON.stringify(data));
-                    }
-                    else
-                    {
-                        alert('error at: ' + response.message);
-                    }
-                }
-            });
+            $scope.searchCustomer();
         }
     }
 });
