3f4d4ed125e6584651d3558e6a1e039ab36042db
Hung Phung <hungphung0808@gmail.com>
2016-01-05 16:55:53 +0700

    Implement Backend for Product Management Bounded-Context

--- ThanhHuongSolution.Product.Domain/Interfaces/IProductManagementRepository.cs
+++ ThanhHuongSolution.Product.Domain/Interfaces/IProductManagementRepository.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ThanhHuongSolution.Product.Domain.Entity;
 
 namespace ThanhHuongSolution.Product.Domain.Interfaces
 {
     public interface IProductManagementRepository
     {
-        Task<bool> CreateCustomer(MDProduct customer);
     }
 }
--- ThanhHuongSolution.Product.Domain/Model/ProductInfo.cs
+++ ThanhHuongSolution.Product.Domain/Model/ProductInfo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ThanhHuongSolution.Product.Domain.Model
 {
-    class ProductInfo
+    public class ProductInfo
     {
     }
 }
