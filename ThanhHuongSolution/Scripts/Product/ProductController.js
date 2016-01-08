0b209dec5a93225d8735c0c7934265e8cd4254ca
Hung Phung <hungphung0808@gmail.com>
2016-01-09 01:31:47 +0700

    Add dropdownlist filter Product type and implement FE

--- ThanhHuongSolution/Scripts/Product/ProductController.js
+++ ThanhHuongSolution/Scripts/Product/ProductController.js
@@ -1,7 +1,91 @@
 ﻿var app = angular.module('ThanhHuongSolution', ['toastr']);
 app.controller('ProductController', function ($scope, toastr, $http) {
+
+    $scope.productTypes = [{ 'key': 'Tất cả', 'value': '0' }, { 'key': 'Lương thực', 'value': '1' }, {'key':'Phân bón', 'value':'2'}];
+
     $scope.init = function (data) {
         $scope.availableProduct = data.LstProduct;
         $scope.products = data.LstProduct;
+        $scope.selectedProductType = $scope.productTypes[0];
+    }
+
+    $scope.search = function () {
+        $scope.query = '';
+
+        $.ajax({
+            type: "POST",
+            url: "/Product/Search",
+            data: { query: $scope.query },
+            success: function (response) {
+                if (response.isSuccess) {
+                    var data = response.data;
+
+                    $scope.availableProduct = data;
+
+                    $scope.searchProduct();
+                }
+                else {
+                    alert('error at: ' + response.message);
+                }
+            }
+        });
+    }
+
+    $scope.searchProduct = function () {
+        $scope.products = [];
+        var query = $scope.query.toLowerCase();
+
+        for (var i = 0; i < $scope.availableProduct.length; i++) {
+            var trackingLower = $scope.availableProduct[i].TrackingNumber.toLowerCase();
+            var nameLower = $scope.availableProduct[i].Name.toLowerCase();
+
+            if (trackingLower.indexOf(query) !== -1 || nameLower.indexOf(query) !== -1)
+                $scope.products.push($scope.availableProduct[i]);
+        }
+    }
+
+    $scope.searchKeyDown = function (event) {
+        if (event.keyCode == 13) {
+            $scope.searchProduct();
+        }
+    }
+    
+    $scope.getProductType = function (type)
+    {
+        switch (type)
+        {
+            case 1: return 'Lương thực';
+            case 2: return 'Phân bón';
+        }
+        return '';
+    }
+
+    $scope.getProductType = function (type) {
+        switch (type) {
+            case 1: return 'Lương thực';
+            case 2: return 'Phân bón';
+        }
+        return '';
+    }
+
+    $scope.getUnitType = function (type) {
+        switch (type) {
+            case 1: return 'Kg';
+        }
+        return '';
+    }
+
+    $scope.onChangeProductType = function (productType)
+    {
+        $scope.products = [];
+
+        if (productType.value == 0)
+            $scope.products = $scope.availableProduct;
+        else {
+            for (var i = 0; i < $scope.availableProduct.length; i++) {
+                if ($scope.availableProduct[i].ProductType == productType.value)
+                    $scope.products.push($scope.availableProduct[i]);
+            }
+        }
     }
 });
