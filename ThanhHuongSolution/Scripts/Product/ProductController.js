f649c02e224014f825225cb8dd156c877b307a82
Hung Phung <hungphung0808@gmail.com>
2016-01-10 15:54:55 +0700

    Apply paging for product page using angular-ui-bootstrap

--- ThanhHuongSolution/Scripts/Product/ProductController.js
+++ ThanhHuongSolution/Scripts/Product/ProductController.js
@@ -1,12 +1,27 @@
-﻿var app = angular.module('ThanhHuongSolution', ['toastr']);
+﻿var app = angular.module('ThanhHuongSolution', ['toastr', 'ui.bootstrap']);
 app.controller('ProductController', function ($scope, toastr, $http) {
 
-    $scope.productTypes = [{ 'key': 'Tất cả', 'value': '0' }, { 'key': 'Lương thực', 'value': '1' }, {'key':'Phân bón', 'value':'2'}];
+    $scope.productTypes = [{ 'key': 'Tất cả', 'value': '0' }, { 'key': 'Lương thực', 'value': '1' }, { 'key': 'Phân bón', 'value': '2' }];
+    $scope.pageSize = 2;
+    $scope.pageIndex = 1;
+    $scope.maxsize = 5;
+    $scope.pagingSource = [];
 
     $scope.init = function (data) {
         $scope.availableProduct = data.LstProduct;
-        $scope.products = data.LstProduct;
+        $scope.pagingSource = $scope.availableProduct;
         $scope.selectedProductType = $scope.productTypes[0];
+        
+        $scope.updatePagingConfig();
+
+        $scope.onChangePageIndex();
+    }
+
+    $scope.updatePagingConfig = function()
+    {
+        $scope.totalServerItems = $scope.pagingSource.length;
+
+        $scope.numPages = Math.ceil($scope.totalServerItems / $scope.pageSize);
     }
 
     $scope.search = function () {
@@ -18,10 +33,16 @@ app.controller('ProductController', function ($scope, toastr, $http) {
 
                 $scope.availableProduct = data;
 
-                $scope.products = data;
+                $scope.pagingSource = data;
+
+                $scope.updatePagingConfig();
+
+                $scope.pageIndex = 1;
+
+                $scope.onChangePageIndex();
             }
             else {
-                alert('error at: ' + response.message);
+                toastr.error('error at: ' + response.message);
             }
         });
     }
@@ -30,7 +51,6 @@ app.controller('ProductController', function ($scope, toastr, $http) {
         if (event.keyCode == 13) {
             $scope.search();
             $scope.selectedProductType = $scope.productTypes[0];
-            //$scope.searchProduct();
         }
     }
     
@@ -61,14 +81,37 @@ app.controller('ProductController', function ($scope, toastr, $http) {
 
     $scope.onChangeProductType = function (productType)
     {
-        $scope.products = [];
+        $scope.pagingSource = [];
 
         if (productType.value == 0)
-            $scope.products = $scope.availableProduct;
+            $scope.pagingSource = $scope.availableProduct;
         else {
             for (var i = 0; i < $scope.availableProduct.length; i++) {
                 if ($scope.availableProduct[i].ProductType == productType.value)
-                    $scope.products.push($scope.availableProduct[i]);
+                    $scope.pagingSource.push($scope.availableProduct[i]);
+            }
+        }
+
+        $scope.pageIndex = 1;
+
+        $scope.updatePagingConfig();
+
+        $scope.onChangePageIndex();
+    }
+
+    $scope.onChangePageIndex = function ()
+    {
+        $scope.products = [];
+        if ($scope.pageIndex == $scope.numPages) {
+            for (var i = ($scope.pageIndex - 1) * $scope.pageSize; i < $scope.pagingSource.length; i++) {
+                $scope.products.push($scope.pagingSource[i]);
+            }
+        }
+        else {
+            for (var i = 0; i < $scope.pageSize; i++)
+            {
+                var index = ($scope.pageIndex - 1) * $scope.pageSize + i;
+                $scope.products.push($scope.pagingSource[index]);
             }
         }
     }
