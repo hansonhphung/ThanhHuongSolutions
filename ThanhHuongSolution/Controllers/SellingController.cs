942400a95aa4fa697f8cb7d48dd6102a8974e168
Hoang <jasonbui692@gmail.com>
2016-01-12 22:42:05 +0700

    Add View Selling

---
+++ ThanhHuongSolution/Controllers/SellingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+
+namespace ThanhHuongSolution.Controllers
+{
+    public class SellingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("Selling");
+        }
+
+        public ActionResult Selling()
+        {
+            return View();
+        }
+    }
+}
