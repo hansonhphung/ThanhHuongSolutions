f54772eb3e6199cbaefb726d6d19661f0c13f65c
Hung Phung <hungphung0808@gmail.com>
2016-01-20 00:35:14 +0700

    Create entity MDBilling for Billing Management

---
+++ ThanhHuongSolution.BillingManagement.Domain/Entity/MDBilling.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThanhHuongSolution.BillingManagement.Domain.Model;
+
+namespace ThanhHuongSolution.BillingManagement.Domain.Entity
+{
+    public class MDBilling
+    {
+        public MDBilling() { }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string TrackingNumber { get; set; }
+
+        public STCustomer Customer { get; set; }
+
+        public long TotalPrice { get; set; }
+
+        public long Discount { get; set; }
+    }
+}
---
+++ ThanhHuongSolution.BillingManagement.Domain/Model/STCustomer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThanhHuongSolution.BillingManagement.Domain.Model
+{
+    public class STCustomer
+    {
+        public string CustomerId { get; set; }
+
+        public string CustomerTrackingNumber { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public STCustomer() { }
+
+        public STCustomer(string customerId, string customerTrackingNumber, string customerName) {
+            CustomerId = customerId;
+            CustomerTrackingNumber = customerTrackingNumber;
+            CustomerName = customerName;
+        }
+    }
+}
