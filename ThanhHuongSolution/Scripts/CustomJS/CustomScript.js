4766cc37c0cbbabcb2a648bdc9bef63e393a7a7f
Jason <jasonbui692@gmail.com>
2016-01-23 21:21:30 +0700

    Style choose file

---
+++ ThanhHuongSolution/Scripts/CustomJS/CustomScript.js
@@ -0,0 +1,21 @@
+﻿//---------- Image (start) ---------------
+$("#btnFile").click(function () {
+    $("#customerImage").click();
+});
+
+function readUrl(input)
+{
+    if (input.files && input.files[0])
+        var reader = new FileReader();
+
+    reader.onload = function (e) {
+        $("#showImage").attr('src', e.target.result);
+    }
+
+    reader.readAsDataURL(input.files[0]);
+}
+
+$("#customerImage").change(function () {
+    readUrl(this);
+});
+//---------- Image (end) ---------------
