656b53bb1e0dc27546c086be02d660b7abf2f429
Jason <jasonbui692@gmail.com>
2016-02-02 22:13:02 +0700

    Implementing Billing View (Cont)

---
+++ ThanhHuongSolution/Scripts/Billing/BillingController.js
@@ -0,0 +1,10 @@
+﻿var app = angular.module('ThanhHuongSolution', ['toastr']);
+
+app.controller('BillingController', function ($scope, toastr, $http) {
+
+    $scope.init = function (data) {
+        alert(JSON.stringify(data));
+        $scope.lstBilling = data.LstBilling;
+        $scope.bills = data.LstBilling;
+    }
+});
