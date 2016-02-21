a17a28fd62158d9ff0112753a6a22b143b8dad64
Hung Phung <hungphung0808@gmail.com>
2016-02-22 01:47:26 +0700

    Add Customer Authorize attribute

---
+++ ThanhHuongSolution/Security/CustomAuthorizeAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThanhHuongSolution.Security
+{
+    public class CustomAuthorizeAttribute : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (string.IsNullOrEmpty(SessionProvider.UserName))
+                filterContext.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary(
+                    new { Controller = "Account", Action = "Index"}));
+        }
+    }
+}
