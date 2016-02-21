69e22a37f2a21506af5aded87992dc3d3219a361
Hung Phung <hungphung0808@gmail.com>
2016-02-22 01:47:01 +0700

    Add session provider used for session management

---
+++ ThanhHuongSolution/Security/SessionProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThanhHuongSolution.Security
+{
+    public class SessionProvider
+    {
+        static string USER_NAME = "username";
+
+        public static string UserName
+        {
+            get {
+                if (HttpContext.Current == null)
+                    return string.Empty;
+
+                var username = HttpContext.Current.Session[USER_NAME];
+                if (username != null)
+                    return username as string;
+
+                return null;
+            }
+
+            set {
+                HttpContext.Current.Session[USER_NAME] = value;
+            }
+        }
+    }
+}
