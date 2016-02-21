f9e8443ffc822938a5319bb4cf28771cf449c1c4
Hung Phung <hungphung0808@gmail.com>
2016-02-22 01:48:03 +0700

    Add password encryption by MD5 service

---
+++ ThanhHuongSolution.Common.Infrastrucure/Utilities/HashUtility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThanhHuongSolution.Common.Infrastrucure.Utilities
+{
+    public class HashUtility
+    {
+        public static string CalculateMD5Hash(string input)
+        {
+            // step 1, calculate MD5 hash from input
+            var md5 = MD5.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string
+            var sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
