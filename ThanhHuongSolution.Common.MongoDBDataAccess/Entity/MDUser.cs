6c3b45e6ba2300826676f79fb136408ec133264e
Hung Phung <hungphung0808@gmail.com>
2016-02-22 01:48:54 +0700

    Add user entity

---
+++ ThanhHuongSolution.Common.MongoDBDataAccess/Entity/MDUser.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThanhHuongSolution.Common.MongoDBDataAccess.Entity
+{
+    public class MDUser
+    {
+        public MDUser() { }
+
+        public MDUser(string id, string username, string password)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+        }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
