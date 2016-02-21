77856f9458faaa4dcb7d2017493f18220f705ae0
Hung Phung <hungphung0808@gmail.com>
2016-02-22 01:51:55 +0700

    Implement Account FE

---
+++ ThanhHuongSolution/Scripts/Account/AccountController.js
@@ -0,0 +1,17 @@
+﻿var app = angular.module('ThanhHuongSolution', ['toastr']);
+
+app.controller('AccountController', function ($scope, toastr, $http) {
+
+    $scope.login = function ()
+    {
+        
+
+        $.ajax({
+            url: "/Account/Login",
+            data: { username: $scope.username, password: $scope.password }
+        }).success(function (response) {
+            location.href = "/Billing/List";
+        });
+    }
+
+});
