09e69b78a21ad9f83e8b0c3b91c3b0e735cf6200
Hung Phung <hungphung0808@gmail.com>
2016-02-23 00:44:36 +0700

    Fix calculating Liability amount when key press

--- ThanhHuongSolution/Scripts/Selling/SellingController.js
+++ ThanhHuongSolution/Scripts/Selling/SellingController.js
@@ -154,7 +154,7 @@ app.controller('SellingController', function ($scope, toastr, $http) {
 
     $scope.numberInputTypeKeyPress = function ($event)
     {
-        if (($event.keyCode >= 48 && $event.keyCode <= 57 ) || $event.keyCode == 8 || $event.keyCode == 46)
+        if (($event.keyCode >= 96 && $event.keyCode <= 105 ) || $event.keyCode == 8 || $event.keyCode == 46)
         {
 
             var payAmount = 0;
