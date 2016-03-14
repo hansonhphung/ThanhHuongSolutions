6a09173f33ffbc296db4975f4296ff1ddde5f08b
Jason <jasonbui692@gmail.com>
2016-03-14 22:41:39 +0700

    Add Receiving view

---
+++ ThanhHuongSolution/Controllers/ReceivingController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ThanhHuongSolution.Controllers
+{
+    public class ReceivingController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ThanhHuongSolution/Scripts/Receiving/ReceivingController.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module('ThanhHuongSolution', ['toastr', 'ui.select', 'ui.bootstrap']);
+app.controller('ReceivingController', function ($scope, toastr, $http) {
+
+    $scope.init()
+    {
+
+    }
+
+});
