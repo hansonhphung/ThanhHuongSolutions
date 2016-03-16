e77b5d43af02d2df13310fb8452d6706d4721b81
Hung Phung <hungphung0808@gmail.com>
2016-03-16 22:23:14 +0700

    include receiving controller js file

--- ThanhHuongSolution/Scripts/Receiving/ReceivingController.js
+++ ThanhHuongSolution/Scripts/Receiving/ReceivingController.js
@@ -1,7 +1,9 @@
-﻿var app = angular.module('ThanhHuongSolution', ['toastr', 'ui.select', 'ui.bootstrap']);
+﻿var app = angular.module('ThanhHuongSolution', ['toastr', 'ui.select']);
 app.controller('ReceivingController', function ($scope, toastr, $http) {
 
-    $scope.init()
+    $scope.pageIndex = 1;
+
+    $scope.init = function()
     {
 
     }
