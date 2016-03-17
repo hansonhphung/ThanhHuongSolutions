fa8e59dfb5a10ed9217476f1b15888a029eca2c5
Hung Phung <hungphung0808@gmail.com>
2016-03-18 01:41:56 +0700

    Implement BE of Receiving page to get data

--- ThanhHuongSolution/Controllers/ReceivingController.cs
+++ ThanhHuongSolution/Controllers/ReceivingController.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using ThanhHuongSolution.Common.Infrastrucure;
+using ThanhHuongSolution.Extension;
+using ThanhHuongSolution.Models.Receiving;
+using ThanhHuongSolution.Models.Selling;
+using ThanhHuongSolution.Notification;
+using ThanhHuongSolution.Product.Domain.Interfaces;
 
 namespace ThanhHuongSolution.Controllers
 {
     public class ReceivingController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            try
+            {
+                var productAPI = WebContainer.Instance.ResolveAPI<IProductManagementAPI>();
+
+                var productData = await productAPI.GetAllProduct();
+
+                var lstProductInfo = productData.Result.Select(x => new ProductInfoModel(x.Id, x.TrackingNumber, x.Name, x.WholesalePrice, x.RetailPrice, x.Number)).ToList();
+
+                var data = new ReceivingInformationModel(lstProductInfo, DateTime.UtcNow.ToString("dd/MM/yyyy"));
+
+                return View("Index", data);
+
+            }
+            catch (CustomException ex)
+            {
+                TempData.AddNotification(NotificationType.Failure, ex.Message);
+                return View("Index");
+            }
         }
     }
 }
---
+++ ThanhHuongSolution/Models/Receiving/ReceivingInformationModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ThanhHuongSolution.Models.Selling;
+
+namespace ThanhHuongSolution.Models.Receiving
+{
+    public class ReceivingInformationModel
+    {
+        public IList<ProductInfoModel> LstProduct { get; set; }
+
+        public string CreateBillDate { get; set; }
+
+        public ReceivingInformationModel() { }
+
+        public ReceivingInformationModel(IList<ProductInfoModel> lstProduct, string createBillDate)
+        {
+            LstProduct = lstProduct;
+            CreateBillDate = createBillDate;
+        }
+    }
+}
