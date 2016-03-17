3ccced541e6451f34a90e7a6155890df508a1fa7
Jason <jasonbui692@gmail.com>
2016-03-18 00:09:50 +0700

    Implement View Receiving Bill (cont)

---
+++ ThanhHuongSolution/Controllers/ReceivingBillController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ThanhHuongSolution.Controllers
+{
+    public class ReceivingBillController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
