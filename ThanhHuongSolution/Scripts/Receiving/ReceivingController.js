309de7b76214b194b570ca2158ea79859f59d40c
Hung Phung <hungphung0808@gmail.com>
2016-03-18 01:42:09 +0700

    Implement FE of Receiving page

--- ThanhHuongSolution/Scripts/Receiving/ReceivingController.js
+++ ThanhHuongSolution/Scripts/Receiving/ReceivingController.js
@@ -1,11 +1,26 @@
 ﻿var app = angular.module('ThanhHuongSolution', ['toastr', 'ui.select']);
 app.controller('ReceivingController', function ($scope, toastr, $http) {
 
+    $scope.shoppingCart = [];
+    $scope.pagingSource = [];
+    $scope.needUpdateProduct = [];
+
+    $scope.maxSize = 1;
+    $scope.recordPerPage = 1;
     $scope.pageIndex = 1;
+    $scope.totalAmount = 0;
+    $scope.incurredCost = 0; //chi phi phat sinh
 
-    $scope.init = function()
-    {
-
+    $scope.init = function (data) {
+        $scope.lstProduct = data.LstProduct;
+        $scope.number = 0;
+        $scope.inputPrice = 0;
+        $scope.createBillDate = data.CreateBillDate;
     }
 
+    $scope.selectProduct = function (selectedProduct) {
+        $scope.selectedProduct = selectedProduct;
+        $scope.inputPrice = 0;
+        $scope.number = 0;
+    }
 });
