e6260bd0117c9af7ffab256a6b617b0a138d8bf8
Hung Phung <hungphung0808@gmail.com>
2016-06-20 00:05:06 +0700

    Create model to handle remaining product Info

---
+++ ThanhHuongSolution.Product.Domain/Model/RemainingProductInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThanhHuongSolution.Product.Domain.Entity;
+
+namespace ThanhHuongSolution.Product.Domain.Model
+{
+    public class RemainingProductInfo
+    {
+        public RemainingProductInfo() { }
+
+        public RemainingProductInfo(string id, string trackingNumber, string name, string description, UnitType unitype, ProductType productType, long price, long quantity)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            UnitType = unitype;
+            ProductType = productType;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public string Id { get; set; }
+        public string TrackingNumber { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public UnitType UnitType { get; set; }
+        public ProductType ProductType { get; set; }
+        public long Price { get; set; }
+        public long Quantity { get; set; }
+    }
+}
