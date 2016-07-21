1185db7c9c54ad5a67720020a59284956d1022bc
Hung Phung <hungphung0808@gmail.com>
2016-07-21 23:33:07 +0700

    Implement API Remaining Product Statistics

--- ThanhHuongSolution/Controllers/StatisticsController.cs
+++ ThanhHuongSolution/Controllers/StatisticsController.cs
@@ -8,6 +8,7 @@ using ThanhHuongSolution.Common.Infrastrucure;
 using ThanhHuongSolution.Customer.Domain.Interfaces;
 using ThanhHuongSolution.Extension;
 using ThanhHuongSolution.Notification;
+using ThanhHuongSolution.Product.Domain.Interfaces;
 
 namespace ThanhHuongSolution.Controllers
 {
@@ -32,6 +33,27 @@ namespace ThanhHuongSolution.Controllers
             {
                 TempData.AddNotification(NotificationType.Failure, ex.Message);
                 return Json(new { isSuccess = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult RemainingProductStatistics() {
+            return View();
+        }
+
+        public async Task<ActionResult> SearchRemainingProduct(string query)
+        {
+            try
+            {
+                var api = WebContainer.Instance.ResolveAPI<IProductManagementAPI>();
+
+                var data = await api.GetAllRemainingProduct(new FrameworkParamInput<string>(query));
+
+                return Json(new { isSuccess = true, data = data.Result}, JsonRequestBehavior.AllowGet);
+            }
+            catch (CustomException ex)
+            {
+                TempData.AddNotification(NotificationType.Failure, ex.Message);
+                return Json(new { isSuccess = false, message = ex.Message}, JsonRequestBehavior.AllowGet);
             }
         }
     }
